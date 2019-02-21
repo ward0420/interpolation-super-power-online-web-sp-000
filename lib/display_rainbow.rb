cb39d875cf0cfca8312a96781fd7468d6a57a669
Don Ward <10is0420@gmail.com>
2019-02-21 12:41:52 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow (colors)
-  letters = []
-  letters << colors
-  puts colors
+  letters = ["R","O","Y","G","B","I","V"]
+  puts "#{letters[0]}: #{colors},
   
 end
