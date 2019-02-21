eb0ad0ca8a6fe614c5a4c9d8b61a45e27af5bdf4
Don Ward <10is0420@gmail.com>
2019-02-21 12:43:13 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow (colors)
   letters = ["R","O","Y","G","B","I","V"]
-  puts "#{letters[0]}: #{colors},
-  
+  puts "#{letters[0]}: #{colors}, #{letters[1]}: #{colors}, #{letters[2]}: #{colors}, #{letters[3]}: #{colors}, #{letters[4]}: #{colors}, #{letters[5]}: #{colors}, #{letters[6]}: #{colors}"
 end
